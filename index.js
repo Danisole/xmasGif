fb7b31f3f669a0469b3b09bd76aad17a74190aff
Danisole <danisole82@gmail.com>
2022-12-24 17:04:02 -0300

    agregando funcionalidades

---
+++ index.js
@@ -0,0 +1,95 @@
+const numRandom = document.getElementById("numRandom")
+const numRandom1 = document.getElementById("numRandom1")
+const numRandom2 = document.getElementById("numRandom2")
+
+
+const personajeRandom = () =>{
+
+    numRandom.innerHTML="";
+    numRandom1.innerHTML="";
+    numRandom2.innerHTML="";
+
+    //Math random para crear un numero aleatorio
+
+    const numeroRandom = Math.floor(Math.random() * xmasCaracters.length) +1
+    console.log(numeroRandom)
+
+    const numCaracRandom = Math.floor(Math.random() * caracteristicas.length) +1
+
+    console.log(numCaracRandom);
+    
+    //Filter para traer un elemento
+    const filterPersonaje = xmasCaracters.filter( (el)=>{
+       
+        return el.id == numeroRandom
+
+    }) 
+    console.log(filterPersonaje);
+
+    //filtrar caracteristica
+
+    const filterCaracteristica = caracteristicas.filter( (el)=>{
+       
+        return el.id == numCaracRandom
+
+    }) 
+    console.log(filterCaracteristica)
+
+    //map para mostrar el elemeto del array generado por el filter
+    //creamos el elemento p para mostrar el mensaje 
+    //generamos el span para el elemento aleatoreo
+
+    filterPersonaje.map((el)=>{
+
+    const pPersonajes = document.createElement("p")
+    pPersonajes.className = "p-personajes";
+
+    pPersonajes.innerHTML=`
+    El personaje que eres esta navidad es 
+    <span>${el.personaje}</span>
+    `
+    numRandom.append(pPersonajes)
+    })
+
+    //filtrar la caracteristica
+
+    filterCaracteristica.map((element)=>{
+
+        const spanCaracteristica = document.createElement("span")
+        spanCaracteristica.className = "span-caract";
+
+        spanCaracteristica.innerHTML=`
+        ${element.caracteristica}`
+        
+        numRandom2.append(spanCaracteristica)
+    })
+
+    //generamos el boton para que comience la magia
+
+    const btnPersonaje = document.createElement("button")
+    
+    btnPersonaje.innerText = "Aprieta"
+    btnPersonaje.className = "btn-personaje"
+
+    const miPersonaje = btnPersonaje.addEventListener("click", personajeRandom)
+    
+    numRandom1.append(btnPersonaje)
+
+}
+personajeRandom()
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
---
+++ personajes.js
@@ -0,0 +1,102 @@
+const xmasCaracters = [
+    {
+        id:1,
+        personaje: "Reno"
+    },
+    {
+        id:2,
+        personaje: "Duende"
+    },
+    {
+        id:3,
+        personaje: "Muñeco de nieve"
+    },
+    {
+        id:4,
+        personaje: "Santa"
+    },
+    {
+        id:5,
+        personaje: "Grinch"
+    },
+    {
+        id:6,
+        personaje: "Pino"
+    },
+    {
+        id:7,
+        personaje: "Galleta de jengibre"
+    },
+    {
+        id:8,
+        personaje: "Angelito"
+    },
+    {
+        id:9,
+        personaje: "Oso polar"
+    },
+    {
+        id:10,
+        personaje: "Mama Noel"
+    },
+    {
+        id:11,
+        personaje: "Elfo"
+    },
+    {
+        id:12,
+        personaje: "Pastorcillo"
+    },
+];
+
+
+const caracteristicas = [
+    {
+        id:1,
+        caracteristica: "Magico"
+    },
+    {
+        id:2,
+        caracteristica: "Esquizofrenico"
+    },
+    {
+        id:3,
+        caracteristica: "Caliente"
+    },
+    {
+        id:4,
+        caracteristica: "Soquete"
+    },
+    {
+        id:5,
+        caracteristica: "Sensual"
+    },
+    {
+        id:6,
+        caracteristica: "Psicopata"
+    },
+    {
+        id:7,
+        caracteristica: "Dormilon"
+    },
+    {
+        id:8,
+        caracteristica: "Bichota"
+    },
+    {
+        id:9,
+        caracteristica: "Bipolar"
+    },
+    {
+        id:10,
+        caracteristica: "Engreido"
+    },
+    {
+        id:11,
+        caracteristica: "Angelical"
+    },
+    {
+        id:12,
+        caracteristica: "Mentiroso"
+    }
+]
